69f726cad693a2660008104678ba63396954c9e1
Sean Morrison & Eric Johnson <student@epicodus.com>
2014-07-30 12:12:10 -0700

    Create Game and Mark Space functionality added

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,23 +1,63 @@
 class Board
-  def initialize
 
+  attr_accessor :spaces
+
+  def initialize
+    @spaces = []
+    create_spaces
   end
+
+  def create_spaces
+    i = 1
+    9.times do
+      new_space = Space.new(i)
+      @spaces << new_space
+      i += 1
+    end
+  end
+
 end
 
 class Space
-  def initialize
+  attr_accessor :marked_by
 
+  def initialize num
+    @space_num = num
+    @marked_by = nil
   end
+
+  def mark_space symbol
+    @marked_by = symbol
+  end
+
 end
 
 class Player
-  def initialize
+  attr_reader :symbol
 
+  def initialize symbol
+    @symbol = symbol
   end
 end
 
 class Game
-  def initialize
+  attr_reader :players
 
+  def initialize
+    create_board
+    add_players
+  end
+
+  def add_players
+    @players = []
+    playerX = Player.new("X")
+    @players << playerX
+    playerY = Player.new("Y")
+    @players << playerY
+  end
+
+  def create_board
+    new_board = Board.new
+    new_board
   end
 end
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,4 +1,5 @@
 require 'rspec'
+require 'pry'
 require 'tic_tac_toe'
 
 describe "Board" do
@@ -6,19 +7,34 @@ describe "Board" do
     test_board = Board.new
     expect(test_board).to be_an_instance_of Board
   end
+
+  it "Creates 9 spaces" do
+    test_board = Board.new
+    expect(test_board.spaces.length).to eq 9
+  end
 end
 
 describe "Space" do
   it "Creates an instance of the Space class" do
-    test_space = Space.new
+    test_space = Space.new(1)
     expect(test_space).to be_an_instance_of Space
+  end
+  it "Marks a space with a symbol" do
+    test_space = Space.new(1)
+    test_space.mark_space("X")
+    expect(test_space.marked_by).to eq "X"
   end
 end
 
 describe "Player" do
   it "Creates an instance of the Player class" do
-    test_player = Player.new
+    test_player = Player.new("X")
     expect(test_player).to be_an_instance_of Player
+  end
+
+  it "Returns the symbol of the given player" do
+    test_player = Player.new("X")
+    expect(test_player.symbol).to eq "X"
   end
 end
 
@@ -26,5 +42,18 @@ describe "Game" do
   it "Creates an instance of the Game class" do
     test_game = Game.new
     expect(test_game).to be_an_instance_of Game
+  end
+
+  it "Creates two players" do
+    test_game = Game.new
+    expect(test_game.players[0].symbol).to eq "X"
+    expect(test_game.players[1].symbol).to eq "Y"
+  end
+
+  it "Creates a board" do
+    test_game = Game.new
+    test_board = test_game.create_board
+    expect(test_board).to be_an_instance_of Board
+    expect(test_board.spaces.length).to eq 9
   end
 end
